4b420c03e00a8a1d7838af70f51d5e71565aeead
Giacomo <giacomo.licari@gmail.com>
2017-11-10 10:44:14 +0100

    Removed custom admin file, let the user defining custom header

--- django_google_authenticator/admin.py
+++ django_google_authenticator/admin.py
@@ -1,13 +1,4 @@
-from django.contrib.admin import AdminSite
-from django.contrib.auth.models import User, Group
+from django.contrib import admin
 from models import Google
 
-class GoogleAdminSite(AdminSite):
-    site_header = 'GnosisDB Administration'
-    # template_name = 'django_google_authenticator/templates/admin/login.html'
-
-admin = GoogleAdminSite(name='GoogleAdmin')
-
-# admin.register(User)
-# admin.register(Group)
-admin.register(Google)
+admin.site.register(Google)
